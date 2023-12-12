954471b9a8538b60d686caa120c1cacc6b7a7b3e
Bernice Awinpang <b.akudbilla@student.com>
2023-12-12 14:09:33 -0800

    task 0

---
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+#webstack debugging 4
+exec { 'ULIMIT':
+  onlyif  => 'test -e /etc/default/nginx',
+  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
